e54258e4a5aedf0682d3cc3d85631861f17f0be1
Erin Hamilton <88404596+ErisianAutomata@users.noreply.github.com>
2024-01-30 21:33:34 +0000

    dbt commit

---
+++ demo_dbt_project/models/example/customers.sql
@@ -0,0 +1,11 @@
+select distinct c.id as customer_id
+     , c.name
+     , c.email
+     , min(o.created_at) as first_order_at
+     , count(o.id)      as number_of_orders
+  from `analytics-engineers-club.coffee_shop.customers` c
+  join `analytics-engineers-club.coffee_shop.orders` o on o.customer_id = c.id
+group by c.id
+     , c.name
+     , c.email
+order by first_order_at;
